d2889d7346f03acd6ddaf82325af6ea2d05d78f4
Alberto Medrano <Alberto Medrano>
2016-10-21 15:39:40 +0000

    init of task MSM

---
+++ app/src/main/java/development/alberto/com/msm_task/MainActivity.java
@@ -0,0 +1,13 @@
+package development.alberto.com.msm_task;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
